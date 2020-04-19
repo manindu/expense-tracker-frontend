1ce768fa87b002a6747d8c7f0368ac1e9e16f3b9
Manindu Wijewickrama <maninduw@Manindus-MacBook-Air.local>
2020-04-19 21:52:08 +0530

    transaction form validation with formik

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,8 +14,19 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Select,
+  NumberInput,
+  NumberInputField,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInputStepper,
 } from "@chakra-ui/core";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const Home = () => {
   const [modalVisible, toggleModal] = useState(false);
@@ -24,6 +35,29 @@ const Home = () => {
     toggleModal(!modalVisible);
   };
 
+  const {
+    values,
+    errors,
+    handleChange,
+    handleSubmit,
+    setFieldValue,
+  } = useFormik({
+    initialValues: {
+      type: "Income",
+      note: "",
+      amount: 0,
+    },
+    validationSchema: Yup.object({
+      type: Yup.string().required(),
+      note: Yup.string().required(),
+      amount: Yup.number().required(),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+      toggleTransactionModal();
+    },
+  });
+
   return (
     <Box display="flex" justifyContent="center">
       <Modal isOpen={modalVisible} onClose={toggleTransactionModal}>
@@ -31,14 +65,54 @@ const Home = () => {
         <ModalContent>
           <ModalHeader>Add Transaction</ModalHeader>
           <ModalBody>
-            <Text>Form</Text>
+            <FormControl isRequired mb="10px">
+              <FormLabel htmlFor="type">Transaction Type</FormLabel>
+              <Select
+                id="type"
+                onChange={(event) => setFieldValue("type", event.target.value)}
+                value={values.type}
+              >
+                <option value="Income">Income</option>
+                <option value="Expense">Expense</option>
+              </Select>
+              <FormErrorMessage>"some error"</FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired mb="10px">
+              <FormLabel htmlFor="note">Note</FormLabel>
+              <Input
+                id="note"
+                placeholder="Note"
+                onChange={handleChange}
+                value={values.note}
+              />
+              <FormErrorMessage>"some error"</FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired mb="10px">
+              <FormLabel htmlFor="amount">Amount</FormLabel>
+              <NumberInput
+                onChange={(value) => setFieldValue("amount", value)}
+                id="amount"
+                precision={2}
+                defaultValue={0}
+                value={values.amount}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+              <FormErrorMessage>"some error"</FormErrorMessage>
+            </FormControl>
           </ModalBody>
 
           <ModalFooter>
             <Button mr={3} onClick={toggleTransactionModal}>
               Close
             </Button>
-            <Button variantColor="green">Add</Button>
+            <Button variantColor="green" onClick={handleSubmit}>
+              Add
+            </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
